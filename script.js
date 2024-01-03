05019b2c8f431c1e812c32551aca3fd988d75410
Natália Oliveira <136612215+naatlr@users.noreply.github.com>
2024-01-03 15:49:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ let secondNumber = prompt("Digite o segundo número:")
 firstNumber = Number(firstNumber)
 secondNumber = Number(secondNumber)
 
-const sun = firstNumber + secondNumber
+const sum = firstNumber + secondNumber
 const sub = firstNumber - secondNumber
 const multi = firstNumber * secondNumber
 const div = firstNumber / secondNumber
 const restDiv = firstNumber % secondNumber
 
-alert("Soma: " + sun)
+alert("Soma: " + sum)
 alert("Subtração: " + sub)
 alert("Miltiplicação: " + multi)
 alert("Divisão: " + div)
 alert("Resto da divisão: " + restDiv)
 
-if(sun%2 == 0){
-	alert("É um número Par: " + sun);
+if(sum%2 == 0){
+	alert("É um número Par: " + sum);
 } else {
-	alert("É um número Impar: " + sun);
+	alert("É um número Impar: " + sum);
 }
 
 if(firstNumber == secondNumber) {
